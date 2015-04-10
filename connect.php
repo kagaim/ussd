ef871fd776927bbdcfaebed02e6e07129a75d49e
Kagai <ekagai@gmail.com>
2015-04-10 18:49:33 +0300

    added the db connection file

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "bima";
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+//print_r($connection);
+
+?>
